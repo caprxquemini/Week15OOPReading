8b068a4e6ac71500ef13c6b0a6fff6ac2bedb14b
caprxquemini <stefaniabarabula12@gmail.com>
2023-05-21 16:58:55 +0300

    Add project files.

---
+++ Week15OOPReading/Program.cs
@@ -0,0 +1,39 @@
+﻿string[] data = GetDataFromFile();
+List<Movie> list = new List<Movie>();
+
+foreach(string line in data)
+{
+    string[] tempArray = line.Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+    Movie newMovie = new Movie(tempArray[0], tempArray[1], tempArray[2]);
+
+    list.Add(newMovie);
+}
+
+foreach(Movie movie in list)
+{
+    Console.WriteLine($"One of my favorite movies: {movie.title} was released in {movie.year}.");
+}
+
+
+static string[] GetDataFromFile()
+{
+    string filePath = @"C:\Users\Stefania\Documents";
+    return File.ReadAllLines(filePath);
+    }
+
+
+
+class Movie
+{
+    public string title;
+    public string rating;
+    public string year;
+
+    public Movie(string _title, string _rating, string _year)
+    {
+        _title = title;
+        _rating = rating;
+        _year = year;
+
+    }
+}
